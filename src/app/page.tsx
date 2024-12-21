09fa9f6749d1ed813a001b1e75ba6789f57b290a
iqra <fhsaleem8104@gmail.com>
2024-12-21 22:55:05 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './views/hero'
 
+
 export default function Home() {
   return (
     <div>
       <Hero/>
+    
     </div>
   )
 }
--- src/app/views/hero.tsx
+++ src/app/views/hero.tsx
@@ -37,15 +37,18 @@ export default function Hero() {
         {/* Overlay */}
         <div className="absolute inset-0 bg-black/40"></div>
         {/* Header */}
-        <header className="absolute top-0 left-0 w-full flex items-center justify-between px-4 py-4 sm:px-6 md:px-8 lg:px-12 z-10">
-          <div className="text-xl sm:text-2xl md:text-3xl text-orange-300 font-cinzel">
+        <header className="fixed top-0 left-0 w-full flex items-center justify-between px-4 py-4 sm:px-6 md:px-8 lg:px-12 z-10 ">
+          <div className="text-xl sm:text-2xl md:text-3xl text-orange-400 font-cinzel">
             Flavor
           </div>
           <nav className="flex space-x-4 sm:space-x-6 md:space-x-8">
-            <Link href="#home" className="text-white hover:text-gray-300">
+            <Link href="/" className="text-white hover:text-gray-300">
               Home
             </Link>
-            <Link href="#menu" className="text-white hover:text-gray-300">
+            <Link href="#why-us" className="text-white hover:text-gray-300">
+              Why Us
+            </Link>
+            <Link href="/menu" className="text-white hover:text-gray-300">
               Menu
             </Link>
             <Link href="#about" className="text-white hover:text-gray-300">
@@ -56,6 +59,7 @@ export default function Hero() {
             </Link>
           </nav>
         </header>
+
         <div
           id="scrollingText"
           className="absolute flex flex-col justify-center items-center top-0 h-full font-cinzel transition-transform duration-300 ease-linear px-4"
@@ -63,7 +67,7 @@ export default function Hero() {
           <h1 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl px-2 sm:px-4 text-center">
             Flavor Restaurant
           </h1>
-          <p className="text-sm sm:text-base md:text-lg text-orange-300 mt-4 px-4 sm:px-6 text-center max-w-[90%] sm:max-w-[80%] md:max-w-[60%] break-words">
+          <p className="text-sm sm:text-base md:text-lg text-orange-400 mt-4 px-4 sm:px-6 text-center max-w-[90%] sm:max-w-[80%] md:max-w-[60%] break-words">
             Flavor Restaurant offers a perfect blend of flavor and ambiance,
             <br />
             serving a variety of mouthwatering dishes crafted
@@ -74,6 +78,50 @@ export default function Hero() {
             Our Menu
           </button>
         </div>
+      </div>
+
+      {/* Sections why-us  */}
+      <div  id="why-us">
+      <div   id="scrollingText" className="flex flex-col items-center mt-10 font-cinzel transition-transform duration-300 ease-linear">
+          <h1 className="text-orange-400 text-xl">Why Us</h1>
+          <h1 className="text-black text-2xl mt-3">Why Choose Our Restaurant</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6 mt-8">
+          <div className="p-6 bg-gray-100 mt-2">
+            01
+            <h1 className="mt-6 text-orange-400 font-cinzel text-xl">Delicious</h1>
+            <p className="mt-4 text-black">
+              At our restaurant, we believe in delivering more than just a meal
+              create unforgettable experiences.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-100 mt-2">
+            01
+            <h1 className="mt-6 text-orange-400 font-cinzel text-xl">Tempting</h1>
+            <p className="mt-4 text-black">
+              At our restaurant, we believe in delivering more than just a meal
+              create unforgettable experiences.
+            </p>
+          </div>
+          <div className="p-6 bg-gray-100 mt-2">
+            01
+            <h1 className="mt-6 text-orange-400 font-cinzel text-xl">
+              Quality Food
+            </h1>
+            <p className="mt-4 text-black">
+              At our restaurant, we believe in delivering more than just a meal
+              create unforgettable experiences.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div id="about" className="h-screen bg-gray-200 flex items-center justify-center">
+        <h2 className="text-3xl">Menu</h2>
+      </div>
+
+      <div id="contact" className="h-screen bg-gray-300 flex items-center justify-center">
+        <h2 className="text-3xl">Contact</h2>
       </div>
     </div>
   );
