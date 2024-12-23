a6084c576807e641a6f45a939cd0b6d5820232c0
iqra <fhsaleem8104@gmail.com>
2024-12-23 23:26:34 +0500

    update

--- src/app/category/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export async function generateStaticParams() {
-    return [{ slug: 'example' }]; // Ensure this is an array of objects
-  }
-  
-  export default function Page({ params }: { params: { slug: string } }) {
-    return <div>My Category: {params.slug}</div>;
-  }
-  
