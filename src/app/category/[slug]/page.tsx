b9148c21eaf07fc1ecf87d106cd811b0f9c8c440
iqra <fhsaleem8104@gmail.com>
2024-12-24 13:21:04 +0500

    update

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -5,12 +5,12 @@ const getmenuItemsByCategory = (category: string) => {
   return menuItems.filter((item) => item.category === category);
 };
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = params;
-  const result = getmenuItemsByCategory(slug);
+export default function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params; // Extract slug from params
+  const result = getmenuItemsByCategory(slug); // Filter menu items by category
 
   return (
-    <div className='grid grid-cols-2 gap-10 mt-20'>
+    <div className="grid grid-cols-2 gap-10 mt-20">
       {result.map((item) => (
         <Menucard
           key={item.id}
